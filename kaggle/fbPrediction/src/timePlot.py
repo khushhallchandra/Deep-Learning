cdb52d1ca4575c385d501719c0a0343e6ee4ee8a
khushhallchandra <kcm00000@gmail.com>
2016-06-05 22:06:44 +0530

    Added

--- kaggle/fbPrediction/src/timePlot.py
+++ kaggle/fbPrediction/src/timePlot.py
@@ -6,8 +6,8 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import matplotlib.pyplot as plt
 
-df_train = pd.read_csv('../input/train.csv')
-df_test = pd.read_csv('../input/test.csv')
+df_train = pd.read_csv('../data/train.csv')
+#df_test = pd.read_csv('../input/test.csv')
 
 
 
